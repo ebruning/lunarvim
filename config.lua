9ca7fc53cb62a96b9befb161ff4c684a7f7ab8d1
Ethan Bruning <ethan@bruning.io>
2022-05-07 07:09:08 -0700

    gruvbox theme

--- config.lua
+++ config.lua
@@ -11,8 +11,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
--- lvim.colorscheme = "onedarker"
-lvim.colorscheme = "gruvbox-material"
+lvim.colorscheme = 'gruvbox-material'
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
@@ -26,7 +25,7 @@ lvim.keys.normal_mode["z"] = ":Telekasten panel<cr>"
 vim.opt.cursorline = false
 
 lvim.line_wrap_cursor_movement = false
-lvim.lsp.diagnostics.virtual_text = false
+lvim.lsp.diagnostics.virtual_text = true
 
 -- unmap a default keymapping
 -- lvim.keys.normal_mode["<C-Up>"] = false
@@ -65,7 +64,7 @@ lvim.builtin.which_key.mappings["t"] = {
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.dashboard.active = true
+-- lvim.builtin.dashboard.active = true
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
@@ -149,128 +148,156 @@ linters.setup {
 
 -- Additional Plugins
 lvim.plugins = {
-    {'sainnhe/gruvbox-material'},
-    {'renerocksai/calendar-vim'},
-    {'nvim-telescope/telescope-media-files.nvim'},
-    {'renerocksai/telekasten.nvim'},
-    {"folke/trouble.nvim"},
+  { "sainnhe/gruvbox-material" },
+  { "renerocksai/calendar-vim" },
+  { "nvim-telescope/telescope-media-files.nvim" },
+  { "renerocksai/telekasten.nvim" },
+  { "folke/trouble.nvim" },
+  { "LunarVim/onedarker" },
 }
 
-require('telescope').load_extension('media_files')
+-- require('telescope').load_extension('media_files')
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- lvim.autocommands.custom_groups = {
 --   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
 -- }
 
-local home = vim.fn.expand("~/zettelkasten")
-require('telekasten').setup({
-    home         = home,
+-- local home = vim.fn.expand("~/zettelkasten")
+-- require('telekasten').setup({
+--    home         = home,
 
-    -- if true, telekasten will be enabled when opening a note within the configured home
-    take_over_my_home = true,
+--    -- if true, telekasten will be enabled when opening a note within the configured home
+--    take_over_my_home = true,
 
-    -- auto-set telekasten filetype: if false, the telekasten filetype will not be used
-    --                               and thus the telekasten syntax will not be loaded either
-    auto_set_filetype = true,
+--    -- auto-set telekasten filetype: if false, the telekasten filetype will not be used
+--    --                               and thus the telekasten syntax will not be loaded either
+--    auto_set_filetype = true,
 
-    -- dir names for special notes (absolute path or subdir name)
-    dailies      = home .. '/' .. 'daily',
-    weeklies     = home .. '/' .. 'weekly',
-    templates    = home .. '/' .. 'templates',
+--    -- dir names for special notes (absolute path or subdir name)
+--    dailies      = home .. '/' .. 'daily',
+--    weeklies     = home .. '/' .. 'weekly',
+--    templates    = home .. '/' .. 'templates',
 
-    -- image (sub)dir for pasting
-    -- dir name (absolute path or subdir name)
-    -- or nil if pasted images shouldn't go into a special subdir
-    image_subdir = "img",
+--    -- image (sub)dir for pasting
+--    -- dir name (absolute path or subdir name)
+--    -- or nil if pasted images shouldn't go into a special subdir
+--    image_subdir = "img",
 
-    -- markdown file extension
-    extension    = ".md",
+--    -- markdown file extension
+--    extension    = ".md",
 
-    -- following a link to a non-existing note will create it
-    follow_creates_nonexisting = true,
-    dailies_create_nonexisting = true,
-    weeklies_create_nonexisting = true,
+--    -- following a link to a non-existing note will create it
+--    follow_creates_nonexisting = true,
+--    dailies_create_nonexisting = true,
+--    weeklies_create_nonexisting = true,
 
-    -- template for new notes (new_note, follow_link)
-    -- set to `nil` or do not specify if you do not want a template
-    template_new_note = home .. '/' .. 'templates/new_note.md',
+--    -- template for new notes (new_note, follow_link)
+--    -- set to `nil` or do not specify if you do not want a template
+--    template_new_note = home .. '/' .. 'templates/new_note.md',
 
-    -- template for newly created daily notes (goto_today)
-    -- set to `nil` or do not specify if you do not want a template
-    template_new_daily = home .. '/' .. 'templates/daily.md',
+--    -- template for newly created daily notes (goto_today)
+--    -- set to `nil` or do not specify if you do not want a template
+--    template_new_daily = home .. '/' .. 'templates/daily.md',
 
-    -- template for newly created weekly notes (goto_thisweek)
-    -- set to `nil` or do not specify if you do not want a template
-    template_new_weekly= home .. '/' .. 'templates/weekly.md',
+--    -- template for newly created weekly notes (goto_thisweek)
+--    -- set to `nil` or do not specify if you do not want a template
+--    template_new_weekly= home .. '/' .. 'templates/weekly.md',
 
-    -- image link style
-    -- wiki:     ![[image name]]
-    -- markdown: ![](image_subdir/xxxxx.png)
-    image_link_style = "markdown",
+--    -- image link style
+--    -- wiki:     ![[image name]]
+--    -- markdown: ![](image_subdir/xxxxx.png)
+--    image_link_style = "markdown",
 
-    -- integrate with calendar-vim
-    plug_into_calendar = true,
-    calendar_opts = {
-        -- calendar week display mode: 1 .. 'WK01', 2 .. 'WK 1', 3 .. 'KW01', 4 .. 'KW 1', 5 .. '1'
-        weeknm = 4,
-        -- use monday as first day of week: 1 .. true, 0 .. false
-        calendar_monday = 1,
-        -- calendar mark: where to put mark for marked days: 'left', 'right', 'left-fit'
-        calendar_mark = 'left-fit',
-    },
+--    -- integrate with calendar-vim
+--    plug_into_calendar = true,
+--    calendar_opts = {
+--        -- calendar week display mode: 1 .. 'WK01', 2 .. 'WK 1', 3 .. 'KW01', 4 .. 'KW 1', 5 .. '1'
+--        weeknm = 4,
+--        -- use monday as first day of week: 1 .. true, 0 .. false
+--        calendar_monday = 1,
+--        -- calendar mark: where to put mark for marked days: 'left', 'right', 'left-fit'
+--        calendar_mark = 'left-fit',
+--    },
 
-    -- telescope actions behavior
-    close_after_yanking = false,
-    insert_after_inserting = true,
+--    -- telescope actions behavior
+--    close_after_yanking = false,
+--    insert_after_inserting = true,
 
-    -- tag notation: '#tag', ':tag:', 'yaml-bare'
-    tag_notation = "#tag",
+--    -- tag notation: '#tag', ':tag:', 'yaml-bare'
+--    tag_notation = "#tag",
 
-    -- command palette theme: dropdown (window) or ivy (bottom panel)
-    command_palette_theme = "ivy",
+--    -- command palette theme: dropdown (window) or ivy (bottom panel)
+--    command_palette_theme = "ivy",
 
-    -- tag list theme:
-    -- get_cursor: small tag list at cursor; ivy and dropdown like above
-    show_tags_theme = "ivy",
+--    -- tag list theme:
+--    -- get_cursor: small tag list at cursor; ivy and dropdown like above
+--    show_tags_theme = "ivy",
 
-    -- when linking to a note in subdir/, create a [[subdir/title]] link
-    -- instead of a [[title only]] link
-    subdirs_in_links = true,
+--    -- when linking to a note in subdir/, create a [[subdir/title]] link
+--    -- instead of a [[title only]] link
+--    subdirs_in_links = true,
 
-    -- template_handling
-    -- What to do when creating a new note via `new_note()` or `follow_link()`
-    -- to a non-existing note
-    -- - prefer_new_note: use `new_note` template
-    -- - smart: if day or week is detected in title, use daily / weekly templates (default)
-    -- - always_ask: always ask before creating a note
-    template_handling = "smart",
+--    -- template_handling
+--    -- What to do when creating a new note via `new_note()` or `follow_link()`
+--    -- to a non-existing note
+--    -- - prefer_new_note: use `new_note` template
+--    -- - smart: if day or week is detected in title, use daily / weekly templates (default)
+--    -- - always_ask: always ask before creating a note
+--    template_handling = "smart",
 
-    -- path handling:
-    --   this applies to:
-    --     - new_note()
-    --     - new_templated_note()
-    --     - follow_link() to non-existing note
-    --
-    --   it does NOT apply to:
-    --     - goto_today()
-    --     - goto_thisweek()
-    --
-    --   Valid options:
-    --     - smart: put daily-looking notes in daily, weekly-looking ones in weekly,
-    --              all other ones in home, except for notes/with/subdirs/in/title.
-    --              (default)
-    --
-    --     - prefer_home: put all notes in home except for goto_today(), goto_thisweek()
-    --                    except for notes with subdirs/in/title.
-    --
-    --     - same_as_current: put all new notes in the dir of the current note if
-    --                        present or else in home
-    --                        except for notes/with/subdirs/in/title.
-    new_note_location = "smart",
+--    -- path handling:
+--    --   this applies to:
+--    --     - new_note()
+--    --     - new_templated_note()
+--    --     - follow_link() to non-existing note
+--    --
+--    --   it does NOT apply to:
+--    --     - goto_today()
+--    --     - goto_thisweek()
+--    --
+--    --   Valid options:
+--    --     - smart: put daily-looking notes in daily, weekly-looking ones in weekly,
+--    --              all other ones in home, except for notes/with/subdirs/in/title.
+--    --              (default)
+--    --
+--    --     - prefer_home: put all notes in home except for goto_today(), goto_thisweek()
+--    --                    except for notes with subdirs/in/title.
+--    --
+--    --     - same_as_current: put all new notes in the dir of the current note if
+--    --                        present or else in home
+--    --                        except for notes/with/subdirs/in/title.
+--    new_note_location = "smart",
 
-    -- should all links be updated when a file is renamed
-    rename_update_links = true,
-})
+--    -- should all links be updated when a file is renamed
+--    rename_update_links = true,
+-- })
 
-
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'gruvbox-material',
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+    disabled_filetypes = {},
+    always_divide_middle = true,
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = { 'filename' },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { 'filename' },
+    lualine_x = { 'location' },
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {}
+}
